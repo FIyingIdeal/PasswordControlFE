12a5ea84fc88b89dbe766691477221992bfb3b31
yanchao <yanchao@hiynn.com>
2017-05-12 19:35:52 +0800

    添加跳转到搜索页

--- src/actions/login/index.js
+++ src/actions/login/index.js
@@ -2,7 +2,7 @@
 * @Author: yanchao
 * @Date:   2017-05-05 16:51:32
 * @Last Modified by:   yanchao
-* @Last Modified time: 2017-05-12 17:59:32
+* @Last Modified time: 2017-05-12 19:31:20
 * @Email: yanchao@hiynn.com
 * @File Path: E:\project\PasswordControlFE\src\actions\login\index.js
 * @File Name: index.js
@@ -13,6 +13,7 @@
 import Ajax from '../../utils/AjaxUtils';
 import UrlUtil from '../../utils/RESTfulApiUtils';
 import ActionTypes from '../../utils/ActionTypeUtils';
+import { push } from 'react-router-redux';
 
 export const login = (values) => dispatch => {
 
@@ -22,6 +23,7 @@ export const login = (values) => dispatch => {
             dispatch({
                 type: ActionTypes.LOGIN, user: data.data
             });
+            dispatch(push('/search'));
         }
     });
 }
